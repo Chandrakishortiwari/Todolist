dddbf74cce004a6e9d1181393f366c6e722e1357
chandrakishor <chandrakishoret197@gmail.com>
2025-02-13 20:21:04 +0530

    Todo data Show in list

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { useState } from 'react'
 function App() {
   const [todolist, setTodolist] = useState([])
 
+  
+  // Add todo value
+
   const fomeHendler = (e)=>{
     e.preventDefault();
 
@@ -17,22 +20,46 @@ function App() {
     }else{
       alert("todo already add")
     }
-
+    e.target.reset();
     console.log(todoname)
   }
   
+
+  let list = todolist.map((value, index)=>{
+     return(
+      <TodoListeItem value={value} />
+     )
+  })
  
 
   return (
     <>
     <h1 className=' text-3xl font-bold'>Todo list</h1>
- 
+     
+     {/* from section */}
+
+
     <form onSubmit={fomeHendler}>
     <input placeholder='name ' name='todo' className='border text-red-500'></input> 
     <button className="bg-cyan-500 ...">Add</button>
     </form>
+
+    {/* Todo list represent section */}
+
+    <ul>
+      {list}
+    
+    </ul>
+
+
     </>
   )
 }
 
-export default App
+export default App;
+
+function TodoListeItem({value}){
+  return(
+    <li> {value}   <button className="bg-cyan-500 ...">Reanme</button> <button className="bg-cyan-500 ...">Delete</button></li>
+  )
+}
